0280e28daa84be075129f7fff9f080a9357c17a8
李金钊 <jinzhao.li@lanmaoly.com>
2017-10-16 16:51:38 +0800

    解决粘包问题

---
+++ src/main/java/com/jzli/netty/demo/chapter4_3/TimeServer.java
@@ -0,0 +1,63 @@
+package com.jzli.netty.demo.chapter4_3;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+
+/**
+ * =======================================================
+ *
+ * @Company 产品技术部
+ * @Date ：2017/10/16
+ * @Author ：李金钊
+ * @Version ：0.0.1
+ * @Description ：解决粘包问题
+ * ========================================================
+ */
+public class TimeServer {
+
+    public void bind(int port) throws InterruptedException {
+        //配置服务器端的NIO线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChildChannelHandler());
+            //绑定端口，同步等待成功
+            ChannelFuture future = serverBootstrap.bind(port).sync();
+            //等待服务器端监听端口关闭
+            future.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new LineBasedFrameDecoder(1024));
+            socketChannel.pipeline().addLast(new StringDecoder());
+            socketChannel.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        int port = 6666;
+        TimeServer timeServer = new TimeServer();
+        timeServer.bind(port);
+    }
+}
